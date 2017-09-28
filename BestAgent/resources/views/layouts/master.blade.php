2aab46fff368a35f7f9e2d287ce2b34b3d0058d6
kvalrie <hugo.barcelona31@gmail.com>
2017-09-28 10:41:58 +0200

    changement css href

--- BestAgent/resources/views/layouts/master.blade.php
+++ BestAgent/resources/views/layouts/master.blade.php
@@ -5,8 +5,8 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
-	<link  href="http://localhost:8888/Unicorn/BestAgent/public/css/css_header.css" rel="stylesheet">
-	<link href="http://localhost:8888/Unicorn/BestAgent/public/css/css_avis.css " rel="stylesheet">
+	<link  href="{{asset('css/css_header.css')}}" rel="stylesheet">
+	<link  href="{{asset('css/css_avis.css')}}" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="UTF-8">
 
@@ -14,6 +14,7 @@
 	<title>Best Agent</title>
 </head>
 <body>
+@include('layouts.nav')
 
 @yield('content')
 
