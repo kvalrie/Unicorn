fe0129483c961a98b7a2bf5454ffcfab5b89a368
kvalrie <hugo.barcelona31@gmail.com>
2017-09-28 11:14:24 +0200

    miner changes

--- BestAgent/resources/views/layouts/nav.blade.php
+++ BestAgent/resources/views/layouts/nav.blade.php
@@ -1,1 +1,15 @@
-
+  <header>
+            <div class="branding">
+           <h1><span>Best</span><span>Agents</span></h1>
+       </div>
+       <div class="nav-icon">
+                        <ul>
+                            <li> <a href="#">Trouver une agence immobilière</a></li>
+                            <li> <a href="#">Donner son avis sur une agence</a></li>
+                            <li id="btn">
+                            <button href="#">Fr</button>
+                            <button href="#">Nl</button>
+                          </li>
+                      </ul>
+                  </div>
+        </header>
--- BestAgent/resources/views/resultat.blade.php
+++ BestAgent/resources/views/resultat.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
 @section('content')
 {{-- include le layout qui affiche les fiches agences basic avec un for each et les conditions de la recherche --}}
-<h1>resulat.blade.php</h1>
+
 @endsection
